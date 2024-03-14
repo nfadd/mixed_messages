7983f6b9ebebbbcc7501545a4f333602f2805d9a
Nick Faddis <nfaddis02@gmail.com>
2024-03-14 15:03:56 -0500

    Add random message generation

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,5 +1,14 @@
-const message = {
+const components = {
     name: ['Mesut Ozil', 'Theo Walcott', 'Aaron Ramsey', 'Thierry Henry', 'Dennis Bergkamp', 'Ian Wright', 'Robert Pires', 'Patrick Viera', 'Bukayo Saka'],
     verb: ['played for', 'coached for', 'scored against', 'got injured against', 'was loaned to'],
     team: ['Arsenal', 'Tottenham', 'Barcelona', 'Real Madrid', 'Napoli', 'AC Milan']
 };
+
+function randomMessage(comps){
+    const nameInd = Math.floor(Math.random() * comps.name.length);
+    const verbInd = Math.floor(Math.random() * comps.verb.length);
+    const teamInd = Math.floor(Math.random() * comps.team.length);
+
+    return `${comps.name[nameInd]} ${comps.verb[verbInd]} ${comps.team[teamInd]}`;
+}
+
