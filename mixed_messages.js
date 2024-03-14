d34693a5affeec8795b7b6dfcf425612b5f8d7e0
Nick Faddis <nfaddis02@gmail.com>
2024-03-14 14:30:53 -0500

    Added random messages object

---
+++ mixed_messages.js
@@ -0,0 +1,5 @@
+const message = {
+    name: ['Mesut Ozil', 'Theo Walcott', 'Aaron Ramsey', 'Thierry Henry', 'Dennis Bergkamp', 'Ian Wright', 'Robert Pires', 'Patrick Viera', 'Bukayo Saka'],
+    verb: ['played for', 'coached for', 'scored against', 'got injured against', 'was loaned to'],
+    team: ['Arsenal', 'Tottenham', 'Barcelona', 'Real Madrid', 'Napoli', 'AC Milan']
+};
