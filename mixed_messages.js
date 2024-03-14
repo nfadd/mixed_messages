6f36b8e4a53522617abbcfaa10e86f8431c82c32
Nick Faddis <nfaddis02@gmail.com>
2024-03-14 15:06:53 -0500

    Print the randomized message to the console for user

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,9 +1,11 @@
+// Store possible message components in object
 const components = {
     name: ['Mesut Ozil', 'Theo Walcott', 'Aaron Ramsey', 'Thierry Henry', 'Dennis Bergkamp', 'Ian Wright', 'Robert Pires', 'Patrick Viera', 'Bukayo Saka'],
     verb: ['played for', 'coached for', 'scored against', 'got injured against', 'was loaned to'],
     team: ['Arsenal', 'Tottenham', 'Barcelona', 'Real Madrid', 'Napoli', 'AC Milan']
 };
 
+// Function to randomly select the components of the message
 function randomMessage(comps){
     const nameInd = Math.floor(Math.random() * comps.name.length);
     const verbInd = Math.floor(Math.random() * comps.verb.length);
@@ -12,3 +14,6 @@ function randomMessage(comps){
     return `${comps.name[nameInd]} ${comps.verb[verbInd]} ${comps.team[teamInd]}`;
 }
 
+// Print the randomized message out to the user
+const message = randomMessage(components);
+console.log(message);
